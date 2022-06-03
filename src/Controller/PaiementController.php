ccd84bb6f0f1bfbe03e8c5837d676c053a382e90
Marvinb67 <brassler.marvin@gmail.com>
2022-06-03 16:26:31 +0200

    DebutSystemedePaiement

---
+++ src/Controller/PaiementController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PaiementController extends AbstractController
+{
+    /**
+     * @Route("/paiement", name="paiement")
+     */
+    public function index(): Response
+    {
+        return $this->render('paiement/index.html.twig', [
+            'controller_name' => 'PaiementController',
+        ]);
+    }
+}
