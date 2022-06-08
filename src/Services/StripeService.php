67f088f8bf6749ddf31231a63523b04cf0616c4f
Marvinb67 <brassler.marvin@gmail.com>
2022-06-08 12:02:27 +0200

    striipe

--- src/Services/StripeService.php
+++ src/Services/StripeService.php
@@ -2,7 +2,32 @@
 
 namespace App\Services;
 
+use Stripe\Stripe;
+use App\Entity\Article;
+use Stripe\PaymentIntent;
+
 class StripeService{
 
-    
+    private $clefPrive;
+
+    public function __construct()
+    {
+        // Si le site est en mode dev on utilise le systeme de paiement test sinon le real
+        if($_ENV['APP_ENV'] === 'dev'){
+            $this->clefPrive = $_ENV['STRIPE_SECRET_KEY_TEST'];
+        }else{
+            $this->clefPrive = $_ENV['STRIPE_SECRET_KEY_REAL'];
+        }
+    }
+
+    // Récupère le prix 
+    public function paymentIntent(Article $article){
+
+        \Stripe\Stripe::setApiKey($this->clefPrive);
+
+        return \Stripe\PaymentIntent::create([
+            'amount'=> $article->getPrix();
+        ]);
+    }
+
 }
