049582794e24a194a9803d08da68053cec256d36
Marvinb67 <brassler.marvin@gmail.com>
2022-06-17 12:03:10 +0200

    vend

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,24 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
+use App\Repository\ArticleRepository;
 use App\Repository\CategorieRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(ManagerRegistry $doctrine, CategorieRepository $ca): Response
+    public function index(ManagerRegistry $doctrine, CategorieRepository $ca, ArticleRepository $ar): Response
     {
         // Je cherche dans le repository Categorie l'ensemble des informations
         $categories = $ca->findAll();
+        $articles = $ar->findBy(["nouveau" => 1]);
+
 
         return $this->render('home/index.html.twig', [
             'categories' => $categories,
+            'articles'=>$articles,
         ]);
     }
 
