f55465e1209b24ee2bad7bf6a430c5738a221b9c
Marvinb67 <johan.kasri@icloud.com>
2022-06-20 12:02:54 +0200

    lundi

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ * @ORM\Table(name="article", indexes={@ORM\Index(columns={"denomination","description"} flags={"fulltext"})})
  */
 class Article
 {
