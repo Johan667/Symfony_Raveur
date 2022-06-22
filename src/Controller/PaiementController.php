d4564d72d468bc81fe9c46f0abc081984ff0c016
Marvinb67 <johan.kasri@icloud.com>
2022-06-22 15:13:38 +0200

    payer

--- src/Controller/PaiementController.php
+++ src/Controller/PaiementController.php
@@ -8,7 +8,9 @@ use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PaiementController extends AbstractController
@@ -19,6 +21,7 @@ class PaiementController extends AbstractController
     // Grace au service SessionInterface je rècupere une session ensuite je met en place un panier qui sera un tableau vide
     public function index(SessionInterface $session, ArticleRepository $articleRepository): Response
     {
+
         $panier = $session->get('panier', []);
         $panierWithData = [];
 
@@ -45,44 +48,54 @@ class PaiementController extends AbstractController
     }
 
     /**
-     * @Route("/payment/{id}/recap", name="payment", methods={"GET","POST"})
-     * @param Article $article
-     * @return Response
+     * @Route("/create-checkout-session", name="checkout")
      */
-    public function payment(Article $article, ProduitController $produitController): Response
-    {
-        if(!$this->getUser()){
-            return $this->redirectToRoute('login');
-        }
-        return $this->render('paiement/index.html.twig', [
-            'user'=>$this->getUser(),
-            'intentSecret'=> $produitController->intentSecret(),
-            'article'=>$article,   
+    public function checkout(SessionInterface $session){
+
+        $panier = $session->get('panier', []);
+        var_dump($panier);die;
+
+        \Stripe\Stripe::setApiKey('sk_test_51L6amqAgDjI611jf49n3RURuEVn6KbawPxt0CKby4wsENM9plWmKeqkq7Cm3Sl1W4JcvjewbvVCBrwyA5knu6b2500QdV5lalL');
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types'=>['card'],
+            // 'line_items' => [[
+            //     # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+            //     'price' => 'price_1LDS0PAgDjI611jfZ58hvHP1',
+            //     'quantity' => 1,
+            // ]],
+            'line_items' => [[
+                # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+                'price' => $panier['total'],
+                'quantity' => $panier['qte'],
+            ]],
+
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'automatic_tax' => [
+            'enabled' => true,
+            ],
         ]);
-}
-    /**
-     * @Route("/payment/commande/{id}/recap", name="payment_commande", methods={"GET","POST"})
-     * @param Article $article
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse\Response
-     * @throws \Exception
-     */
-    public function commande(Article $article, ProduitController $produitController, Request $request){
 
-        $user = $this->getUser();
-
-        if($request->getMethod() === "POST"){
-            $ressource = $produitController->stripe($_POST, $article);
-
-            if(null !== $ressource){
-            $produitController->creation_commande($ressource, $article, $user);
-
-            return $this->render('paiement/reponse.html.twig', [
-                'article'=>$article
-            ]);
-            }
-        }
-        return $this->redirectToRoute('payment',['id'=>$article->getId()]);
+        return new JsonResponse(['id' => $session->id]);
+   
     }
+
+    /**
+     * @Route("/success", name="success")
+     */
+    public function success(){
+        return $this->render('paiement/success.html.twig');
+    }
+
+        /**
+     * @Route("/error", name="error")
+     */
+    public function error(){
+        return $this->render('paiement/error.html.twig');
+    }
+
+
+
 
 }
