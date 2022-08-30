7c2ed1ad6ecde84392978896fd8b3a45f31ae9a5
Johan Kasri <johan.kasri@icloud.com>
2022-08-30 10:03:18 +0200

    formulaire account

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,20 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserType;
 use App\Form\ResetPasswordType;
+use App\Form\UserType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/user", name="app_user")
      */
-    public function index(ManagerRegistry $doctrine, Request $request): Response
+    public function index(ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $user = $this->getUser();
         $form = $this->createForm(UserType::class, $user);
@@ -30,25 +31,25 @@ class UserController extends AbstractController
                  ->setTelephone($form->get('telephone')->getData())
                  ->setSociete($form->get('societe')->getData());
             $doctrine->getManager()->flush(); // on applique les modification
-            $this->addFlash('sucess','Vos informations ont bien été mis a jour !');
-            return $this->redirectToRoute("app_user"); 
+            $this->addFlash('sucess', 'Vos informations ont bien été mis a jour !');
+
+            return $this->redirectToRoute('app_user');
         }
 
-            $password->handleRequest($request);
-            if ($reset->isSubmitted() && $reset->isValid()) {
-                $oldPass = $password->get('oldPassword')->getData();
-                $newPass = $password->get('newPassword')->getData();
-                if (password_verify($oldPass, $user->getPassword())) { // on verifie que l'ancien mot de passe coresponde au nouveau
-                    $user->setPassword( $userPasswordHasher->hashPassword($user, $newPass) ); // on hash le mot de passe et on l'associe a l'utilisateur
+        $password->handleRequest($request);
+        if ($password->isSubmitted() && $password->isValid()) {
+            $oldPass = $password->get('oldPassword')->getData();
+            $newPass = $password->get('newPassword')->getData();
+            if (password_verify($oldPass, $user->getPassword())) { // on verifie que l'ancien mot de passe coresponde au nouveau
+                    $user->setPassword($userPasswordHasher->hashPassword($user, $newPass)); // on hash le mot de passe et on l'associe a l'utilisateur
                     $doctrine->getManager()->flush(); // on l'enregistre
-                    $this->addFlash('sucess','Votre mot de passe a bien été mis a jour !');
-                    return $this->redirectToRoute("app_user"); 
-                
+                    $this->addFlash('sucess', 'Votre mot de passe a bien été mis a jour !');
+            }
+        }
 
         return $this->render('user/index.html.twig', [
             'user' => $user,
             'form' => $form->createView(),
             'password' => $password->createView(), ]);
-    
-
-        }
+    }
+}
